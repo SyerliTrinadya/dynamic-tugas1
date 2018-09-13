bf6e6e5e4e57d181cd6b2e4057e49f1877334c97
User <User@LAPTOP-NNIHKQG5>
2018-09-13 12:54:55 +0700

    Initial Commit

---
+++ src/HitungFibonacci/HitungFibonacci.java
@@ -0,0 +1,16 @@
+
+package HitungFibonacci;
+import java.util.Scanner;
+public class HitungFibonacci {
+   private static void tampilJudul(String identitas ) {
+       System.out.println("identitas"+ identitas);
+       
+       System.out.println("\nHitung Fibonacci");
+       System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+   }
+   public static void main(String[] args) {
+       String identitas = "SYERLI TRINADYA / XRPL 2 / 33";
+       
+       tampilJudul(identitas);
+   } 
+}
