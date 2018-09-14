d21df5205838362ccc7b1c683b4a37af7b8a643f
User <User@LAPTOP-NNIHKQG5>
2018-09-14 21:17:55 +0700

    tambah input

--- src/HitungFibonacci/Tugas01.java
+++ src/HitungFibonacci/Tugas01.java
@@ -5,5 +5,13 @@ public class Tugas01 {
     public static void main(String[] args) 
     {
         String identitas = "SYERLI TRINADYA / XRPL 2 / 33";
+        
+        int[] nominal = new int [] {5000, 2000, 1000, 500, 100};
+        int uang, i,jumlah, n = nominal.length;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah uang : ");
+        uang = scan.nextInt();
+        
+
     }
 }
